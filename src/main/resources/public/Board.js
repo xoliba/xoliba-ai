86962259a52393a81e048a30ce859f6b03d113bc
Mikko Rinta-Homi <mikkojrh@gmail.com>
2017-05-18 11:38:03 +0300

    Board variables are now private

--- src/main/resources/public/Board.js
+++ src/main/resources/public/Board.js
@@ -1,25 +1,24 @@
 const ROWS = 7;
 
-var boardTable = new Array(ROWS);
 
 class Board {
 
     constructor() {
-        this.boardTable;
+        this.boardTable = new Array(ROWS);
         this.create2DArray();
     }
 
     create2DArray() {
         for (let i = 0; i < ROWS; i++) {
-            boardTable[i] = new Array(ROWS);
+            this.boardTable[i] = new Array(ROWS);
             for (let j = 0; j < ROWS; j++) {
-                boardTable[i][j] = 0;
+                this.boardTable[i][j] = 0;
             }
         }
     }
 
     get gameBoard() {
-        return boardTable;
+        return this.boardTable;
     }
 
 }
