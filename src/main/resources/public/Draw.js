f5c9309112b7642129eb1fed96cb618d5f31e464
Mikko Rinta-Homi <mikkojrh@gmail.com>
2017-05-18 11:54:38 +0300

    Draw.js variables are now private (except const variables)

--- src/main/resources/public/Draw.js
+++ src/main/resources/public/Draw.js
@@ -3,17 +3,17 @@ const ypx = 40
 const size = 15
 
 function drawTable(stage, t) {
-    for (var i = 0; i < t.length; i++) {
-        var row = t[i];
-        for (var j = 0; j < row.length; j++) {
-            var color = row[j]
+    for (let i = 0; i < t.length; i++) {
+        let row = t[i];
+        for (let j = 0; j < row.length; j++) {
+            let color = row[j]
             drawButton(stage, j, i, color)
         }
     }
 }
 
 function drawButton(stage, x, y, color) {
-    var graphics = new PIXI.Graphics();
+    let graphics = new PIXI.Graphics();
     if (color == 1) {
         graphics.beginFill(0xe74c3c);
     } else if (color == 0) {
