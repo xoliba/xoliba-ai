01084bf14eb005b6553bf6a31695d3a64b1708ca
Eero Pukkila <eeropukkila@hotmail.com>
2017-05-19 14:17:19 +0300

    Changed to application and sprites on spot with functionality

--- src/main/resources/public/Draw.js
+++ src/main/resources/public/Draw.js
@@ -5,64 +5,17 @@ function drawTable(stage, gameBoard) {
     size = scale();
     let t = gameBoard.boardTable
 
-    drawLines(stage, size, graphics)
+    drawLines(stage, size, graphics);
 
-    for (var i = 0; i < t.length; i++) {
-        let row = t[i];
-        for (var j = 0; j < row.length; j++) {
-            let color = row[j]
-            drawButton(stage, j, i, color, size, graphics, gameBoard)
-        }
-    }
-}
-
-    function drawButton(stage, x, y, color, size, graphics, gameBoard) {
-    if(color < -1) {
-        return
-    }
-
-    padding = size / 10
-    px = size / 7.5;
-    radius = px / 4;
-    if (color == 1) {
-        graphics.beginFill(0xe74c3c); //red
-        graphics.lineStyle(1, 0x000000);
-    } else if (color == 0) {
-        graphics.beginFill(0xffffff); //white
-        graphics.lineStyle(1, 0x000000);
-    } else if (color == -1) {
-        graphics.beginFill(0x0000ff); //blue
-        graphics.lineStyle(1, 0x000000);
-    }
-
-    graphics.drawCircle(padding + x * px, padding + y * px, radius);
-    graphics.endFill();
-
-    //doesnt work
-    /*
-    var texture = graphics.generateTexture();
-    buttonSprite = new PIXI.Sprite(texture);
-    buttonSprite.buttonMode = true;
-    buttonSprite.interactive = true;
-    buttonSprite.hitArea = new PIXI.Circle(px + x * px, px + y * px, radius);
-    buttonSprite.mouseover = function (e) {
-        console.log(x, y);
-    };
-    buttonSprite.on('click', function(a) {
-        console.log("click");
-        gameBoard.clickStone(x,y);
-        buttonSprite.x += 15
-    });
-
-    stage.addChild(buttonSprite);
-    */
     stage.addChild(graphics)
+    return t;
+
 }
 
 function scale() {
     var x = window.innerWidth;
     var y = window.innerHeight;
-    
+
     var result = Math.min(x, y);
     return result;
 }
--- src/main/resources/public/Launcher.js
+++ src/main/resources/public/Launcher.js
@@ -2,32 +2,54 @@
 
 function start() {
     var size = scale()
-    var renderer = PIXI.autoDetectRenderer(size, size, {view: document.getElementById("gameboard")});
-    renderer.backgroundColor = 0xE5E3DF;
+    var app = new PIXI.Application(size, size, {view: document.getElementById("gameboard")});
+    app.renderer.backgroundColor = 0xE5E3DF;
 //Add the canvas to the HTML document
 
 //Create a container object called the `stage`
-    var stage = new PIXI.Container();
+//    var stage = new PIXI.Container();
 
     var gameBoard = new Board();
-    drawTable(stage, gameBoard);
+    var buttons = drawTable(app.stage, gameBoard);
 
-    spritesetup(stage, renderer);
+    var padding = size / 10;
+    var px = size / 7.5;
+    var radius = px / 4;
 
-}
 
-function spritesetup (stage, renderer){
-  PIXI.loader
-    .add("images/whiteCircle64.png")
-    .load(setup);
+      PIXI.loader
+      .add("images/whiteCircle64.png")
+      .load(setup);
 
-  function setup() {
-    var sprite = new PIXI.Sprite(
-      PIXI.loader.resources["images/whiteCircle64.png"].texture
-    );
+      function setup() {
+        for (var i = 0; i < 7; i++) {
+          for (var j = 0; j < 7; j++) {
+            if(!((i == 0 || i == 6) && (j == 0 || j == 6))){
+              var sprite = new PIXI.Sprite(
+                PIXI.loader.resources["images/whiteCircle64.png"].texture
+              );
+              sprite.interactive = true;
+              sprite.buttonMode = true;
+              sprite.x = padding + i * px - radius;
+              sprite.y = padding + j * px - radius;
+              sprite.width = radius * 2;
+              sprite.height = radius * 2;
 
-    stage.addChild(sprite);
-    renderer.render(stage);
+              sprite.on('click', onClick)
 
-  }
+              function onClick(){
+                this.x += 20;
+                console.log('testi')
+              }
+
+              app.stage.addChild(sprite);
+
+            }
+          }
+        }
+      }
+
+
+
+      app.renderer.render(app.stage);
 }
