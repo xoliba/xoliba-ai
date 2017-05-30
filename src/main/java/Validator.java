630489ce7485785d1afd05f4a35b4dc27c3d3305
Vili <vili.hatonen@helsinki.fi>
2017-05-30 14:32:15 +0300

    validator infra is ready to be implemented!

--- src/main/java/Validator.java
+++ src/main/java/Validator.java
@@ -48,32 +48,55 @@ public class Validator {
      * [ [x1,y1], [x2, y2], ...]
      */
     private ArrayList<int[]> getConnectedWhites(int originX, int originY) {
-        int[] origin = new int[]{originX, originY};
-        int[][] walkingDirections = { {1,0},{1,-1},{0,-1},{-1,-1},{-1,0},{-1,1},{0,1},{1,1} }; //east, south-east, south, ...
+        int[][] walkingDirections = { {1,0},{1,1},{0,1},{-1,1},{-1,0},{-1,-1},{0,-1},{1,-1} }; //east, south-east, south, ...
         boolean[] stopWalkingToThisDirection = new boolean[8];
 
         ArrayList<int[]> coordinates = new ArrayList<>();
         for (int i = 1; i < 7; i++) { //the max distance to walk is 6 steps
+            if (areAllDirectionsBlocked(stopWalkingToThisDirection)) {
+                break;
+            }
+
             for (int j = 0; j < 8; j++) { //check all eight directions
                 if (stopWalkingToThisDirection[j]) {
                     continue; //this direction is blocked already
                 }
 
-                int[] coordinateToCheck = { originX + i * walkingDirections[j][0], originY + i * walkingDirections[j][1]};
+                int x = originX + i * walkingDirections[j][0]; //the origin + distance to walk on x-axis to this direction
+                int y = originY + i * walkingDirections[j][1];
+                int[] coordinateToCheck = {x,y};
 
                 if (isThisOffBoard(coordinateToCheck) || board[coordinateToCheck[0]][coordinateToCheck[1]] != 0) {
                     stopWalkingToThisDirection[j] = true;
                     continue;
                 }
-                //the coordinate is on board and white
-            }
 
+                //the coordinate is on board and white, and this direction is not blocked
+                coordinates.add(coordinateToCheck);
+            }
         }
 
         return coordinates;
     }
 
-    private boolean isThisOffBoard(int[] coordinate) {
+    /**
+     *
+     * @param stopWalkingInThisDirection table of booleans
+     * @return are all the 8 directions blocked?
+     */
+    protected boolean areAllDirectionsBlocked(boolean[] stopWalkingInThisDirection) {
+        int blocked = 0;
+        for (int i = 0; i < stopWalkingInThisDirection.length; i++) {
+            if (stopWalkingInThisDirection[i]) {
+                blocked++;
+            }
+        }
+
+        if (blocked == stopWalkingInThisDirection.length) return true;
+        return false;
+    }
+
+    protected boolean isThisOffBoard(int[] coordinate) {
         return false;
     }
 
@@ -83,7 +106,7 @@ public class Validator {
      * @param c2 coordinate that is on a same horizontal, vertical or diagonal axis as the coordinate 1
      * @return if the two are connected by white stones
      */
-    private boolean checkIfConnectedByWhites(int[] c1, int[] c2) {
+    protected boolean checkIfConnectedByWhites(int[] c1, int[] c2) {
         return true;
     }
 
@@ -92,7 +115,7 @@ public class Validator {
      * @param coordinate [x,y]
      * @return can a triangle be formed with the stone at the coordinate
      */
-    private boolean checkIfFormsATriangle(int[] coordinate) {
+    protected boolean checkIfFormsATriangle(int[] coordinate) {
         int color = board[coordinate[0]][coordinate[1]];
         return true;
     }
