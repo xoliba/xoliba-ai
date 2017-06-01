6792333cda9ee7eee49d499d22820d9df3c0d172
Eero Pukkila <eeropukkila@hotmail.com>
2017-06-01 11:37:49 +0300

    Testfiles added for Move and StoneCollector

---
+++ src/test/java/AI/StoneCollectorTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AI;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author eerop
+ */
+public class StoneCollectorTest {
+    
+    public StoneCollectorTest() {
+    }
+        
+    @Before
+    public void setUp() {
+    }
+
+    
+}
