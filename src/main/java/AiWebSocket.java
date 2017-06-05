8e8795b8e107b630708936a6c5d9414fb7c6ef73
Vili <vili.hatonen@helsinki.fi>
2017-06-05 15:53:29 +0300

    removed logging sessions from client connected

--- src/main/java/AiWebSocket.java
+++ src/main/java/AiWebSocket.java
@@ -13,7 +13,7 @@ public class AiWebSocket {
 	@OnWebSocketConnect
 	public void connected(Session session) {
 		sessions.add(session);
-		System.out.println("client connected, session:\n" + session.toString());
+		System.out.println("client connected");
 	}
 
 	@OnWebSocketClose
