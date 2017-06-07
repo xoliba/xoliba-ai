62906bc3128010367b3fa61ac3778d542fd72c4a
rajaro <jaro.ravila@gmail.com>
2017-06-07 13:04:01 +0300

    ai send table fixed with refactored front

--- src/main/java/AiWebSocket.java
+++ src/main/java/AiWebSocket.java
@@ -26,8 +26,33 @@ public class AiWebSocket {
 			System.out.println("ping");
 			return;
 		}
+
 		
 		System.out.println("Got: " + JsonConverter.jsonify(JsonConverter.parseMessage(message)));
+
+		System.out.println("got a message @ " + new java.util.Date());
+
+		handleTableSendTurnData(session, message);
+		//handleData(session, message);
+	}
+
+	private void handleTable(Session session, String message) throws IOException {
+		System.out.println("Got: " + JsonConverter.jsonify(JsonConverter.parseTable(message)) + "\n");
+
+		AI ai = new AI(1);
+		session.getRemote().sendString(JsonConverter.jsonify(ai.move(JsonConverter.parseTable(message)).board));
+	}
+
+	private void handleTableSendTurnData(Session session, String message) throws IOException {
+		System.out.println("Got: " + JsonConverter.jsonify(JsonConverter.parseTable(message)) + "\n");
+
+		AI ai = new AI(1);
+		session.getRemote().sendString(JsonConverter.jsonify(ai.move(JsonConverter.parseTable(message))));
+	}
+
+	private void handleData(Session session, String message) throws IOException {
+		System.out.println("Got: " + JsonConverter.jsonify(JsonConverter.parseMessage(message)) + "\n");
+
 		
         AI ai = new AI(1);
 		session.getRemote().sendString(JsonConverter.jsonify(ai.move(JsonConverter.parseTable(message))));
