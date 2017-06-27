0234e682b06dcb689e252e465ba5380730469035
Mikko Rinta-Homi <mikkojrh@gmail.com>
2017-06-27 11:46:42 +0300

    Apparently settings.gradle is necessary on some machines?

--- src/main/java/OptimizationMain.java
+++ src/main/java/OptimizationMain.java
@@ -6,18 +6,18 @@ import AI.*;
 import static AI.AI.bestParameters;
 
 public class OptimizationMain {
-
     public static void main(String[] args) {
+        /*
         new Coach(true).runOneShowdown(3, bestParameters,
                 3, new ParametersAI(5,40,40,5,5,5),
                 1);
-        //*/
-        /*
+        */
+        ///*
         boolean[] testTheseParameters = new boolean[]{
                 true, true, true, true, true, true};
         new Coach(true).iterateWithDifferentParameters(testTheseParameters, 0, 120, 20,
                 50, 2, 2);
-        */
+        //*/
     }
 
 }
