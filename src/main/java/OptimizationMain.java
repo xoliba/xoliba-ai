68c222ad348dec48d5f306cf2a8ca31b454d74c2
Vili <vili.hatonen@helsinki.fi>
2017-06-29 10:42:31 +0300

    new wait estimation to coach

--- src/main/java/OptimizationMain.java
+++ src/main/java/OptimizationMain.java
@@ -7,19 +7,19 @@ import static AI.AI.bestParameters;
 
 public class OptimizationMain {
     public static void main(String[] args) {
-        /*
-        new Coach(true).runOneShowdown(2, bestParameters,
+        ///*
+        new Coach(true).runOneShowdown(4, bestParameters,
                 4,
                 //new ParametersAI(100,1,1,1,1,1),
                 new AI(1).getBestParameters(4),
-                2);
-        */
-        ///*
+                75);
+        //*/
+        /*
         boolean[] testTheseParameters = new boolean[]{
                 true, true, true, true, true, false};
         new Coach(true).iterateWithDifferentParameters(testTheseParameters, 1, 61, 30,
                 50, 4, 4);
-        //*/
+        */
     }
 
 }
