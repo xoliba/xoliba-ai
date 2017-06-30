7910794185b9374f49dd2aa0875f76152876a6ba
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2017-06-30 17:28:02 +0300

    updated upload script. still in bar.

--- awsUpload.sh
+++ awsUpload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 eval $(aws ecr get-login --no-include-email --region us-east-2)
-docker build -t xoliba-ai-aws .
+docker build --no-cache -t xoliba-ai-aws .
 docker tag xoliba-ai-aws:latest $AWS_ACCOUNT_ID.dkr.ecr.us-east-2.amazonaws.com/xoliba-docker-repo:ai
 docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-2.amazonaws.com/xoliba-docker-repo:ai
